4723308db7fa3fd2a6f2d37349df3778925dbe3c
lutaii <ivanlutaienko@gmail.com>
2023-12-02 18:02:11 +0100

    add colors for light theme, text font, text theme

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors();
+
+  final blue = const Color(0xFF2892F0);
+  final blueDark = const Color(0xFF3054AA);
+  final purple = const Color(0xFFB16CFF);
+  final grey = const Color(0xFF929BA3);
+
+  final background = const Color(0xFFE8F3FC);
+
+  final spacer = const Color(0xFFBCB5B5);
+
+  final icon = const Color(0xFF2892F0);
+  final iconSelected = const Color(0xFF3054AA);
+
+  final historyOn = const Color(0xFF2BEDB3);
+  final historyOff = const Color(0xFFFF6E6E);
+
+  final card = const Color(0xFFFFFFFF);
+  final cardPickerOn = const Color(0xFFC3DBFF);
+  final cardSelectedGradient1 = const Color(0xFF5667FF);
+  final cardSelectedGradient2 = const Color(0xFF2396EF);
+
+  final pickerOn = const Color(0xFF2892F0);
+  final pickerOff = const Color(0xFF929BA3);
+  final pickerGradient1 = const Color(0xFFF71983);
+  final pickerGradient2 = const Color(0xFFFF8D52);
+}
